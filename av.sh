01072002c5afc29d83b81dbe91ae6a740e173456
Nicolas Noury <atrus.nn@gmail.com>
2019-01-07 17:54:40 +0100

    make avsh work again

--- av.sh
+++ av.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-avshCmd="adb shell am broadcast -a com.archos.mediacenter.AVSH -e cmd '$@'"
+avshCmd="adb shell am broadcast -a com.archos.mediacenter.AVSH -e cmd '$@' org.courville.nova/com.archos.mediacenter.LibAvosReceiver"
 $avshCmd > /dev/null
