2fe907a1a9b1cbaa3654b0d581d9bf321efe623d
Marc de Courville <marc@courville.org>
2019-12-08 22:22:42 +0100

    get changelog from internal.txt

--- release/push.py
+++ release/push.py
@@ -16,11 +16,12 @@ repo = g.get_organization("nova-video-player").get_repo("aos-AVP")
 print(repo.name)
 
 #changelog is in whatsnew
-#with open('whatsnew', 'r') as whatsnew:
+with open('../../Video/src/noamazon/play/release-notes/en-US/internal.txt', 'r') as whatsnew:
+    changelog=whatsnew.read()
 #    changelog=whatsnew.read().replace('\n', '')
 
 #create_git_release(tag, name, message, draft=False, prerelease=False, target_commitish=NotSet)
-release = repo.create_git_release(sys.argv[1], sys.argv[2], "Release notes", draft = True)
+release = repo.create_git_release(sys.argv[1], sys.argv[2], changelog, draft = True)
 folder = sys.argv[3]
 print("Made release " + str(release))
 for f in os.listdir(folder):
