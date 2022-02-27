aa8827a6cf1f2932292015b3a714b7c719d46515
Courville Software <software@courville.org>
2022-02-27 18:55:28 +0100

    github release has acra

--- release/release.sh
+++ release/release.sh
@@ -26,7 +26,7 @@ ret=0
 #launch build only if manifests are not the same
 #if [ `diff -q $bdir/manifest.xml $bdir/manifest-new.xml | grep -qE .` ]
 #then
-./gradlew -Poneapk -Psponsor -PadultScrape aNR || ret = 1
+./gradlew -Poneapk -Pacra -Psponsor -PadultScrape aNR || ret = 1
   [ -f $bdir/manifest.xml ] && mv $bdir/manifest.xml $bdir/manifest-old.xml
 #else
   ret=1
