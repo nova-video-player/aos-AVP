e55fcb9b1bd3de862d7348f04a4518e6534749da
Courville Software <software@courville.org>
2022-03-11 21:21:02 +0100

    manifest needs to be created after the build otherwise is erased
    simplify as well

--- release/release.sh
+++ release/release.sh
@@ -10,30 +10,21 @@ cd ../..
 prefix=`pwd`
 avp=$prefix/AVP
 cd $avp
-#git tag -a "v${major}.${minor}" -m "v${major}.${minor} release"
-#git push --tags
-#repo forall -c git tag -a "v${version}" -m "v${version} release"
-#repo forall -c git push --tags
 
 echo "Uploading release v${version} to github..."
 
 cd $prefix/Video
 bdir=$prefix/Video/build/outputs/apk/noamazon/release
 mkdir -p $bdir
-repo manifest -r > $bdir/manifest-new.xml
 
 ret=0
-#launch build only if manifests are not the same
-#if [ `diff -q $bdir/manifest.xml $bdir/manifest-new.xml | grep -qE .` ]
-#then
-./gradlew -Poneapk -Pacra -Psponsor -PadultScrape aNR || ret = 1
-  [ -f $bdir/manifest.xml ] && mv $bdir/manifest.xml $bdir/manifest-old.xml
-#else
-  ret=1
-#fi
+./gradlew -Poneapk -Pacra -Psponsor -PadultScrape aNR && ret=1
+echo "build result: $ret"
 if [ "$ret" == "1" ]
 then
-  mv $bdir/manifest-new.xml $bdir/manifest.xml
+  repo manifest -r > $bdir/manifest.xml
   cd $prefix/AVP/release
-  ./push.py "v${version}" "v${version} release" $bdir
+  ls $bdir
+  echo uploading
+  #./push.py "v${version}" "v${version} release" $bdir
 fi
