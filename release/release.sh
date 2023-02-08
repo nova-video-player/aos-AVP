5e64112748c90aa358bdecad40d916d93903ef00
Courville Software <software@courville.org>
2023-02-08 21:40:18 +0100

    release needs to be aligned too

--- release/release.sh
+++ release/release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #version=$1
-version=$(grep " novaVersionName = '" ../../Video/build.gradle | sed "s/^.*novaVersionName = '\([0-9\.]*\)'.*$/\1/g")
+version=$(grep " versionName = '" ../../Video/build.gradle | sed "s/^.*versionName = '\([0-9\.]*\)'.*$/\1/g")
 major=$(echo $version | cut -d. -f1)
 minor=$(echo $version | cut -d. -f2)
 patch=$(echo $version | cut -d. -f3)
 echo version ${major}.${minor}.${patch}
+
 cd ../..
 prefix=`pwd`
 avp=$prefix/AVP
